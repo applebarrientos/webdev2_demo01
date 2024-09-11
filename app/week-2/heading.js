ccd0beb8073418efafb0a8b3507bfd66a3b98311
applebarrientos <apple.barrientos@edu.sait.ca>
2024-09-11 12:14:45 -0600

    added week 2 demo page
    added new page

---
+++ app/week-2/heading.js
@@ -0,0 +1,8 @@
+export default function Heading(){
+    return (
+        <header>
+            <h1>this is a heading</h1>
+
+        </header>
+    );
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,18 @@
+import Heading from "./heading";
+import Link from "next/link";
+
+export default function Page(){
+    let a = 5;
+    let b = 10;
+    return (
+        <main>
+            <Heading />            
+            <Heading />
+            <Heading />
+            <h1>week 2 demos</h1>
+            <p>these are demos for week 2</p>
+            <p>the sum of a + b is {a + b}.</p>
+            <p>go back to <Link href="/">the home page</Link></p>
+        </main>
+    )
+}
